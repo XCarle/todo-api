987ec1d9f94db5c669b752ac45b45055572dfb55
Alexandre Carle <alexandre@carteldelart.com>
2017-11-21 16:54:36 +0100

    add mongodb logic

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const express = require('express')
 const app = express()
+const mongoClient = require('mongodb').MongoClient
+
+mongoClient.connect("mongodb://localhost:27017/todoDB", function(err, db) {
+  if(!err) {
+    console.log("We are connected")
+  } else {
+    console.log("Error connecting : ", err)
+  }
+
+  var collection = db.collection('todoDB');
+  var doc1 = {'hello':'doc1'};
+  var doc2 = {'hello':'doc2'};
+  var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+
+  collection.insert(doc1,)
+
+  collection.insert(doc2, {w:1}, function(err, result) {});
+
+  collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
+
+  collection.find().toArray(function(err, items) {
+    if (err) {
+      console.log('error while inserting : ', err)
+    } else {
+      console.log('Items : ', items)
+    }
+  })
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
